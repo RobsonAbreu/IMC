dbf9fbf65720b2b1c0bc99eab313ff9fdd0c697a
unknown <Aluno0319@LABF323447.fuvs.br>
2015-05-12 20:38:45 -0300

    added initial file

---
+++ exemplo.js
@@ -0,0 +1,15 @@
+/*
+ * Este é o JavaScript Scratchpad.
+ *
+ * Escreva JavaScript, e depois clique com o botão direito ou selecione no menu Execução:
+ * 1. Executar: para avaliar o texto selecionado (Ctrl+R),
+ * 2. Inspecionar: para abrir o resultado em uma janela de inspeção (Ctrl+I)
+ * 3. Visualizar: para inserir o resultado em um comentário depois da seleção. (Ctrl+L)
+ */
+
+
+for (var i = 1;i<11;i++){
+  console.log('num: '+i);
+}
+
+
